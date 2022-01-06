84f2710ba6e0aa7c6075951166e8798dcc4efd20
Durgesh094 <durgeshchavan1994@gmail.com>
2022-01-06 15:03:14 +0530

    Added: Calculate Total Fare

---
+++ src/test/java/com/briz/cabinvoicegenerator/CabInvoiceServiceTest.java
@@ -0,0 +1,21 @@
+package com.briz.cabinvoicegenerator;
+
+import org.junit.Assert;
+import org.junit.Test;
+import service.InvoiceService;
+
+public class CabInvoiceServiceTest {
+    @Test
+    public void givenDistanceAndTime_whenCalculateFare_ShouldReturnTotalFare() {
+        InvoiceService invoiceService = new InvoiceService();
+        double totalFare = invoiceService.calculateFare(2.0, 5);
+        Assert.assertEquals(25, totalFare, 0.0);
+    }
+
+    @Test
+    public void givenDistanceAndTimeTestCase2_whenCalculateFare_ShouldReturnTotalFare() {
+        InvoiceService invoiceService = new InvoiceService();
+        double totalFare = invoiceService.calculateFare(1.0, 5);
+        Assert.assertEquals(15, totalFare, 0.0);
+    }
+}
