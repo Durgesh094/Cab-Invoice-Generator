05610175b481a4f9d55f5287f2e064ee183a73fb
Durgesh094 <durgeshchavan1994@gmail.com>
2022-01-06 15:26:37 +0530

    Added: Claculation of Multiple Rides and Test Cases

--- src/main/java/service/InvoiceService.java
+++ src/main/java/service/InvoiceService.java
@@ -1,12 +1,26 @@
 package service;
 
+import model.InvoiceSummary;
+import model.Ride;
+
 public class InvoiceService {
     private final double COST_PER_KM = 10.0;
     private final int COST_PER_MIN = 1;
     private final double MIN_FARE = 5.0;
 
+
     public double calculateFare(double distance, int time) {
         double totalFare = distance * COST_PER_KM + time * COST_PER_MIN;
         return Math.max(totalFare, MIN_FARE);
     }
+
+    public InvoiceSummary calculateFare(Ride[] rides) {
+        double totalFare = 0.0;
+        for (Ride ride : rides) {
+            totalFare += calculateFare(ride.getDistance(), ride.getTime());
+        }
+        System.out.println("length " + rides.length + " fare" + totalFare);
+        return new InvoiceSummary(rides.length, totalFare);
+    }
 }
+
--- src/test/java/com/briz/cabinvoicegenerator/CabInvoiceServiceTest.java
+++ src/test/java/com/briz/cabinvoicegenerator/CabInvoiceServiceTest.java
@@ -18,4 +18,11 @@ public class CabInvoiceServiceTest {
         double totalFare = invoiceService.calculateFare(1.0, 5);
         Assert.assertEquals(15, totalFare, 0.0);
     }
+
+    @Test
+    public void givenDistanceAndTime_whenCalculateFareIsLessThan5_ShouldReturnTotalFareAs5() {
+        InvoiceService invoiceService = new InvoiceService();
+        double totalFare = invoiceService.calculateFare(0.1, 1);
+        Assert.assertEquals(5, totalFare, 0.0);
+    }
 }
