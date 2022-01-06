3d14924fedf3aec2ca699bec34335e479e1c6bb1
Durgesh094 <durgeshchavan1994@gmail.com>
2022-01-06 14:41:00 +0530

    Added: Cab Invoice Generator

---
+++ src/main/java/com/briz/cabinvoice/CabInvoice.java
@@ -0,0 +1,10 @@
+package com.briz.cabinvoice;
+
+
+public class CabInvoice {
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO CAB INVOICE GENERATOR PROGRAM");
+    }
+
+
+}
